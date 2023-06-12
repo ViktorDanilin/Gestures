d722b4f1295527d216dbcaa3fee55bef26e34998
ViktorDanilin <danilinvs58@gmail.com>
2023-06-12 19:52:44 +0300

    add ASR with using tensorflow

--- FingerCounter.py
+++ FingerCounter.py
@@ -37,9 +37,9 @@ while True:
         print(totalFingers)
         h, w, c = overlayList[totalFingers - 1].shape
         img[0:h, 0:w] = overlayList[totalFingers - 1]
-        # cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
-        # cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
-        #             10, (255, 0, 0), 25)
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
+                    10, (255, 0, 0), 25)
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
